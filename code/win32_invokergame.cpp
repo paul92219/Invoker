e22c96f58515bd12aa61d598cf5b93f562324f9a
paul92219 <119276586+paul92219@users.noreply.github.com>
2022-12-27 21:48:31 -0800

    Enforcing video frame rate

--- code/win32_invokergame.cpp
+++ code/win32_invokergame.cpp
@@ -25,13 +25,17 @@ typedef uint64_t uint64;
 typedef float real32;
 typedef double real64;
 
-#include <windows.h>
+#include "invoker.cpp"
+#include "invoker.h"
 
+#include <windows.h>
+#include <stdio.h>
 #include "win32_invokergame.h"
 
 // TODO(casey): This is a global now
 global_variable bool32 GlobalRunning;
 global_variable win32_offscreen_buffer GlobalBackBuffer;
+global_variable int64 GlobalPerfCountFrequency;
 
 internal win32_window_dimension
 Win32GetWindowDimension(HWND Window)
@@ -45,28 +49,6 @@ Win32GetWindowDimension(HWND Window)
 
     return(Result);
 }
-/*
-  internal void
-  RenderWeirdGradient(win32_offscreen_buffer *Buffer, int BlueOffset, int GreenOffset)
-  {
-  // TODO(casey): Let's see what the optimizer does
-    
-  uint8 *Row = (uint8 *)Buffer->Memory;
-  for(int Y = 0; Y < Buffer->Height; ++Y)
-  {
-  uint32 *Pixel = (uint32 *)Row;
-  for(int X = 0; X < Buffer->Width; ++X)
-  {
-  uint8 Blue = (X + BlueOffset);
-  uint8 Green = (Y + GreenOffset);
-
-  *Pixel++ = ((Green << 8) | Blue);
-  }
-
-  Row += Buffer->Pitch;
-  }
-  }
-*/
 
 internal void
 Win32ResizeDIBSection(win32_offscreen_buffer *Buffer, int Width, int Height)
@@ -179,6 +161,22 @@ Win32MainWindowCallBack(HWND Window,
     return(Result);
 }
 
+                    
+inline LARGE_INTEGER
+Win32GetWallClock(void)
+{
+    LARGE_INTEGER Result;
+    QueryPerformanceCounter(&Result);
+    return(Result);
+}
+
+inline real32
+Win32GetSecondsElapsed(LARGE_INTEGER Start, LARGE_INTEGER End)
+{
+    real32 Result = ((real32)(End.QuadPart - Start.QuadPart) /
+                     (real32)GlobalPerfCountFrequency);
+    return(Result);
+}
 
 int CALLBACK
 WinMain(HINSTANCE Instance,
@@ -186,6 +184,15 @@ WinMain(HINSTANCE Instance,
         LPSTR CommandLine,
         int ShowCode)
 {
+
+    LARGE_INTEGER PerfCountFrequencyResult;
+    QueryPerformanceFrequency(&PerfCountFrequencyResult);
+    GlobalPerfCountFrequency = PerfCountFrequencyResult.QuadPart;
+
+    // NOTE(casey): Set the Windows scheduler granularity to 1ms
+    // so that our Sleep() can be more granular.
+    UINT DesiredSchedulerMS = 1;
+    bool32 SleepIsGranular = (timeBeginPeriod(DesiredSchedulerMS) == TIMERR_NOERROR);
     
     WNDCLASS WindowClass = {};
     
@@ -196,6 +203,11 @@ WinMain(HINSTANCE Instance,
     WindowClass.hInstance = Instance;
 //  WindowClass.hIcon;
     WindowClass.lpszClassName = "InvokerShooterWindowsClass";
+
+    //TODO(casey): How do we reliably query this on window?
+#define MonitorRefreshHz 60
+#define GameUpdateHz (MonitorRefreshHz / 2)
+    real32 TargetSecondsPerFrame = 1.0f / (real32)GameUpdateHz;
 
     if(RegisterClass(&WindowClass))
     {
@@ -219,21 +231,106 @@ WinMain(HINSTANCE Instance,
         if(Window)
         {
             HDC DeviceContext = GetDC(Window);
+
+
+#if INVOKER_INTERNAL
+            LPVOID BaseAddress = (LPVOID)Terabytes(2);
+#else
+            LPVOID BaseAddress = 0;
+#endif            
             
-            while(GlobalRunning)
+            game_memory GameMemory = {};
+            GameMemory.PermanentStorageSize = Megabytes(64);
+            GameMemory.TransientStorageSize = Gigabytes(1);
+
+            uint64 TotalSize = GameMemory.PermanentStorageSize + GameMemory.TransientStorageSize;
+            GameMemory.PermanentStorage = VirtualAlloc(BaseAddress, (size_t)TotalSize,
+                                                       MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
+            GameMemory.TransientStorage = ((uint8 *)GameMemory.PermanentStorage +
+                                           GameMemory.PermanentStorageSize);
+
+            if(GameMemory.PermanentStorage && GameMemory.TransientStorage)
             {
-                MSG Message;
-                
-                while(PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
+                LARGE_INTEGER LastCounter = Win32GetWallClock();
+
+                uint64 LastCycleCount = __rdtsc();
+
+                while(GlobalRunning)
                 {
-                    if(Message.message == WM_QUIT)
+                    MSG Message;
+                
+                    while(PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
                     {
-                        GlobalRunning = false;
-                    }
+                        if(Message.message == WM_QUIT)
+                        {
+                            GlobalRunning = false;
+                        }
                     
-                    TranslateMessage(&Message);
-                    DispatchMessageA(&Message);
-                }                
+                        TranslateMessage(&Message);
+                        DispatchMessageA(&Message);
+                    }
+
+                    GameUpdateAndRender(&GameMemory);
+                                        
+                    LARGE_INTEGER WorkCounter = Win32GetWallClock();
+                    real32 WorkSecondsElapsed = Win32GetSecondsElapsed(LastCounter, WorkCounter);
+
+                    // TODO(casey): NOT TESTED YET!!! PROBABLY BUGGY!!!
+                    real32 SecondsElapsedForFrame = WorkSecondsElapsed;
+                    if(SecondsElapsedForFrame < TargetSecondsPerFrame)
+                    {
+                        if(SleepIsGranular)
+                        {
+                            DWORD SleepMS = (DWORD)(1000.0f * (TargetSecondsPerFrame -
+                                                               SecondsElapsedForFrame));
+                            if(SleepMS > 0)
+                            {
+                                Sleep(SleepMS);
+                            }
+                            
+                        }
+
+                        // TODO(handy): TargetSecondsPerFrame
+                        //sometimes bigger than  TestSecondsElapsedForFrame??
+
+                            
+                        real32 TestSecondsElapsedForFrame =
+                            (Win32GetSecondsElapsed(LastCounter, Win32GetWallClock()));
+                        real32 Epsilon = 0.0011f; // milliseconds
+
+                            
+                        Assert(TestSecondsElapsedForFrame < TargetSecondsPerFrame + Epsilon);
+                        
+                        while(SecondsElapsedForFrame < TargetSecondsPerFrame)
+                        {
+
+                            SecondsElapsedForFrame = Win32GetSecondsElapsed(LastCounter,
+                                                                            Win32GetWallClock());                        
+                        }
+                    }
+                    else
+                    {
+                        // TODO(casey): MISSED FRAMERATE!
+                        // TODO(casey): Logging
+                    }
+
+                    LARGE_INTEGER EndCounter = Win32GetWallClock();
+                    real32 MSPerFrame = 1000.0f * Win32GetSecondsElapsed(LastCounter, EndCounter);
+                    LastCounter = EndCounter;
+                                        
+                    uint64 EndCycleCount = __rdtsc();
+                    uint64 CyclesElapsed = EndCycleCount - LastCycleCount;
+                    LastCycleCount = EndCycleCount;
+                       
+                    real64 FPS = 0.0f;
+                    real64 MCPF = ((real64)CyclesElapsed / (1000.0f * 1000.0f));
+
+                    char B[256];
+                    _snprintf_s(B, sizeof(B),
+                                "%.02fms/f,  %.02ff/s,  %.02fmc/f\n", MSPerFrame, FPS, MCPF);
+                    OutputDebugStringA(B);                
+
+                }
             }
         }
         else
